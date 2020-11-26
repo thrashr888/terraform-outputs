4d9167ffcde4801ded37e1fb3e03cb4409093c47
Paul Thrasher <pthrasher@hashicorp.com>
2020-11-25 16:48:16 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -33,6 +33,36 @@ output "a-map" {
   description = "The private integer of the main server instance."
 }
 
+output "an-ip-address" {
+  value       = "127.0.0.1"
+  description = "The private url of the local server instance."
+}
+
+output "a-consul-domain" {
+  value       = "redis.consul:6379"
+  description = "The consul url of the main db instance."
+}
+
+output "a-url" {
+  value       = "https://sub.domain.com:8080/path?query=val&2=1#h1"
+  description = "The private url of the main server instance."
+}
+
+output "an-ftp-url" {
+  value       = "ftp://sub.domain.com:8081/path?query=val&2=1"
+  description = "The private ftp url of the main server instance."
+}
+
+output "an-ssh-url" {
+  value       = "ssh://git@github.com:thrashr888/terraform-outputs.git"
+  description = "The private ssh url of the main server instance."
+}
+
+output "a-bash-command" {
+  value       = "gh repo clone thrashr888/terraform-random"
+  description = "The private command of the main server instance."
+}
+
 output "a-long-string" {
   value       = "The private integer of the main server instance is where you want to go when you have the most fun in every Terraform instance you can see in the world that you live in except for dogs because they don't run servers in the same place that humans do."
   description = "A long string is good for you."
@@ -46,6 +76,31 @@ output "a-big-int" {
 output "a-big-decimal" {
   value       = 1234567890.0987654321
   description = "A big decimal is good for you."
+}
+
+output "empty-string" {
+  value       = ""
+  description = "Empty string ''"
+}
+
+output "empty-number" {
+  value       = 0
+  description = "Empty 0"
+}
+
+output "empty-list" {
+  value       = []
+  description = "Empty []"
+}
+
+output "empty-map" {
+  value       = {}
+  description = "Empty {}"
+}
+
+output "a-token" {
+  value       = "233b604b-b92e-48c8-a253-5f11514e4b50"
+  description = "A token is good for you."
 }
 
 output "a-sensitive-value" {
